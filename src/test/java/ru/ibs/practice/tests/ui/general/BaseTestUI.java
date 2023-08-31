506d8e9b952cb515f54fb343141519416ff801c7
Aleksandr <a.ustiantcev@gmail.com>
2023-08-31 19:52:32 +0300

    feat: SELENIUM practical task

--- src/test/java/ru/ibs/practice/tests/ui/general/BaseTestUI.java
+++ src/test/java/ru/ibs/practice/tests/ui/general/BaseTestUI.java
@@ -1,5 +1,38 @@
 package ru.ibs.practice.tests.ui.general;
 
-public class BaseTestUI {
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.ibs.practice.config.SeleniumConfig;
 
+import java.time.Duration;
+
+public class BaseTestUI {
+    protected static WebDriver driver;
+    protected static WebDriverWait wait;
+    protected static ChromeOptions chromeOptions;
+    protected static SeleniumConfig config;
+
+    @BeforeAll
+    public static void setUp() {
+        config = new SeleniumConfig();
+
+        System.setProperty(config.getChromeDriver(), config.getChromeDriverPath());
+
+        chromeOptions = new ChromeOptions();
+        chromeOptions.setBinary(config.getChromeBrowserPath());
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterAll
+    public static void close() {
+        driver.quit();
+    }
 }
